24974050908ad8a9e2813658892403480f14d6f2
William Chang <wilicw1346@gmail.com>
2022-08-02 09:44:58 +0800

    assign buffer with direct index

--- Core/Src/st25r95.c
+++ Core/Src/st25r95.c
@@ -91,10 +91,10 @@ void st25r95_reset() {
 }
 
 st25r95_status_t st25r95_IDN() {
-  tx_len = 0;
-  tx_buffer[tx_len++] = ST25_SEND;
-  tx_buffer[tx_len++] = ST25_IDN;
-  tx_buffer[tx_len++] = 0;
+  tx_buffer[0] = ST25_SEND;
+  tx_buffer[1] = ST25_IDN;
+  tx_buffer[2] = 0;
+  tx_len = 3;
 
   st25r95_nss(1);
   st25r95_spi_tx();
@@ -107,13 +107,12 @@ st25r95_status_t st25r95_IDN() {
 }
 
 st25r95_status_t st25r95_off() {
-  tx_len = 0;
-  tx_buffer[tx_len++] = ST25_SEND;
-  tx_buffer[tx_len++] = ST25_PS;
-  tx_len++;
-  tx_buffer[tx_len++] = ST25_PROTOCOL_OFF;
-  tx_buffer[tx_len++] = 0;
-  tx_buffer[2] = tx_len - 3;
+  tx_buffer[0] = ST25_SEND;
+  tx_buffer[1] = ST25_PS;
+  tx_buffer[2] = 2;
+  tx_buffer[3] = ST25_PROTOCOL_OFF;
+  tx_buffer[4] = 0;
+  tx_len = 5;
 
   st25r95_nss(1);
   st25r95_spi_tx();
@@ -124,13 +123,12 @@ st25r95_status_t st25r95_off() {
 }
 
 st25r95_status_t st25r95_14443A(st25r95_rate_t tx_rate, st25r95_rate_t rx_rate) {
-  tx_len = 0;
-  tx_buffer[tx_len++] = ST25_SEND;
-  tx_buffer[tx_len++] = ST25_PS;
-  tx_len++;
-  tx_buffer[tx_len++] = ST25_PROTOCOL_14443A;
-  tx_buffer[tx_len++] = tx_rate << 6 | rx_rate << 4;
-  tx_buffer[2] = tx_len - 3;
+  tx_buffer[0] = ST25_SEND;
+  tx_buffer[1] = ST25_PS;
+  tx_buffer[2] = 2;
+  tx_buffer[3] = ST25_PROTOCOL_14443A;
+  tx_buffer[4] = tx_rate << 6 | rx_rate << 4;
+  tx_len = 5;
 
   st25r95_nss(1);
   st25r95_spi_tx();
@@ -143,13 +141,13 @@ st25r95_status_t st25r95_14443A(st25r95_rate_t tx_rate, st25r95_rate_t rx_rate) 
 }
 
 st25r95_status_t st25r95_read_reg(uint8_t address, uint8_t *data) {
-  tx_len = 0;
-  tx_buffer[tx_len++] = ST25_SEND;
-  tx_buffer[tx_len++] = ST25_RR;
-  tx_buffer[tx_len++] = 0x3;
-  tx_buffer[tx_len++] = address;
-  tx_buffer[tx_len++] = 0x1;
-  tx_buffer[tx_len++] = 0;
+  tx_buffer[0] = ST25_SEND;
+  tx_buffer[1] = ST25_RR;
+  tx_buffer[2] = 0x3;
+  tx_buffer[3] = address;
+  tx_buffer[4] = 0x1;
+  tx_buffer[5] = 0;
+  tx_len = 6;
 
   st25r95_nss(1);
   st25r95_spi_tx();
@@ -161,14 +159,14 @@ st25r95_status_t st25r95_read_reg(uint8_t address, uint8_t *data) {
 }
 
 st25r95_status_t st25r95_write_timerw(uint8_t data) {
-  tx_len = 0;
-  tx_buffer[tx_len++] = ST25_SEND;
-  tx_buffer[tx_len++] = ST25_WR;
-  tx_buffer[tx_len++] = 0x4;
-  tx_buffer[tx_len++] = 0x3A;
-  tx_buffer[tx_len++] = 0x0;
-  tx_buffer[tx_len++] = data;
-  tx_buffer[tx_len++] = 0x4;
+  tx_buffer[0] = ST25_SEND;
+  tx_buffer[1] = ST25_WR;
+  tx_buffer[2] = 0x4;
+  tx_buffer[3] = 0x3A;
+  tx_buffer[4] = 0x0;
+  tx_buffer[5] = data;
+  tx_buffer[6] = 0x4;
+  tx_len = 7;
 
   st25r95_nss(1);
   st25r95_spi_tx();
@@ -179,13 +177,13 @@ st25r95_status_t st25r95_write_timerw(uint8_t data) {
 }
 
 st25r95_status_t st25r95_write_ARC_index(uint8_t index) {
-  tx_len = 0;
-  tx_buffer[tx_len++] = ST25_SEND;
-  tx_buffer[tx_len++] = ST25_WR;
-  tx_buffer[tx_len++] = 0x3;
-  tx_buffer[tx_len++] = 0x68;
-  tx_buffer[tx_len++] = 0x0;
-  tx_buffer[tx_len++] = index;
+  tx_buffer[0] = ST25_SEND;
+  tx_buffer[1] = ST25_WR;
+  tx_buffer[2] = 0x3;
+  tx_buffer[3] = 0x68;
+  tx_buffer[4] = 0x0;
+  tx_buffer[5] = index;
+  tx_len = 6;
 
   st25r95_nss(1);
   st25r95_spi_tx();
@@ -196,14 +194,14 @@ st25r95_status_t st25r95_write_ARC_index(uint8_t index) {
 }
 
 st25r95_status_t st25r95_write_ARC(uint8_t index, uint8_t data) {
-  tx_len = 0;
-  tx_buffer[tx_len++] = ST25_SEND;
-  tx_buffer[tx_len++] = ST25_WR;
-  tx_buffer[tx_len++] = 0x4;
-  tx_buffer[tx_len++] = 0x68;
-  tx_buffer[tx_len++] = 0x1;
-  tx_buffer[tx_len++] = index;
-  tx_buffer[tx_len++] = data;
+  tx_buffer[0] = ST25_SEND;
+  tx_buffer[1] = ST25_WR;
+  tx_buffer[2] = 0x4;
+  tx_buffer[3] = 0x68;
+  tx_buffer[4] = 0x1;
+  tx_buffer[5] = index;
+  tx_buffer[6] = data;
+  tx_len = 7;
 
   st25r95_nss(1);
   st25r95_spi_tx();
@@ -214,9 +212,9 @@ st25r95_status_t st25r95_write_ARC(uint8_t index, uint8_t data) {
 }
 
 st25r95_status_t st25r95_echo() {
-  tx_len = 0;
-  tx_buffer[tx_len++] = ST25_SEND;
-  tx_buffer[tx_len++] = ST25_ECHO;
+  tx_buffer[0] = ST25_SEND;
+  tx_buffer[1] = ST25_ECHO;
+  tx_len = 2;
 
   st25r95_nss(1);
   st25r95_spi_tx();
@@ -261,12 +259,12 @@ uint8_t st25r95_14443A_detect(uint8_t *ret_uid) {
 }
 
 void st25r95_14443A_REQA(uint8_t *data) {
-  tx_len = 0;
-  tx_buffer[tx_len++] = ST25_SEND;
-  tx_buffer[tx_len++] = ST25_SR;
-  tx_buffer[tx_len++] = 0x2;
-  tx_buffer[tx_len++] = REQA;
-  tx_buffer[tx_len++] = 7; // REQA is a 7bits command.
+  tx_buffer[0] = ST25_SEND;
+  tx_buffer[1] = ST25_SR;
+  tx_buffer[2] = 0x2;
+  tx_buffer[3] = REQA;
+  tx_buffer[4] = 7; // REQA is a 7bits command.
+  tx_len = 5;
 
   st25r95_nss(1);
   st25r95_spi_tx();
@@ -282,13 +280,13 @@ void st25r95_14443A_REQA(uint8_t *data) {
 const static uint8_t cascade_level[] = {CL_1, CL_2, CL_3};
 
 void st25r95_14443A_ANTICOLLISION(uint8_t level, uint8_t *data) {
-  tx_len = 0;
-  tx_buffer[tx_len++] = ST25_SEND;
-  tx_buffer[tx_len++] = ST25_SR;
-  tx_buffer[tx_len++] = 0x03;
-  tx_buffer[tx_len++] = cascade_level[level];
-  tx_buffer[tx_len++] = 0x20; // NVB
-  tx_buffer[tx_len++] = 0x08;
+  tx_buffer[0] = ST25_SEND;
+  tx_buffer[1] = ST25_SR;
+  tx_buffer[2] = 0x03;
+  tx_buffer[3] = cascade_level[level];
+  tx_buffer[4] = 0x20; // NVB
+  tx_buffer[5] = 0x08;
+  tx_len = 6;
 
   st25r95_nss(1);
   st25r95_spi_tx();
@@ -301,18 +299,18 @@ void st25r95_14443A_ANTICOLLISION(uint8_t level, uint8_t *data) {
 }
 
 void st25r95_14443A_select(uint8_t level, uint8_t *data, uint8_t uid0, uint8_t uid1, uint8_t uid2, uint8_t uid3) {
-  tx_len = 0;
-  tx_buffer[tx_len++] = ST25_SEND;
-  tx_buffer[tx_len++] = ST25_SR;
-  tx_buffer[tx_len++] = 0x08;
-  tx_buffer[tx_len++] = cascade_level[level];
-  tx_buffer[tx_len++] = 0x70;
-  tx_buffer[tx_len++] = uid0;
-  tx_buffer[tx_len++] = uid1;
-  tx_buffer[tx_len++] = uid2;
-  tx_buffer[tx_len++] = uid3;
-  tx_buffer[tx_len++] = uid0 ^ uid1 ^ uid2 ^ uid3;
-  tx_buffer[tx_len++] = tx_flag_AppendCRC | 8;
+  tx_buffer[0] = ST25_SEND;
+  tx_buffer[1] = ST25_SR;
+  tx_buffer[2] = 0x08;
+  tx_buffer[3] = cascade_level[level];
+  tx_buffer[4] = 0x70;
+  tx_buffer[5] = uid0;
+  tx_buffer[6] = uid1;
+  tx_buffer[7] = uid2;
+  tx_buffer[8] = uid3;
+  tx_buffer[9] = uid0 ^ uid1 ^ uid2 ^ uid3;
+  tx_buffer[10] = tx_flag_AppendCRC | 8;
+  tx_len = 11;
 
   st25r95_nss(1);
   st25r95_spi_tx();
@@ -325,24 +323,24 @@ void st25r95_14443A_select(uint8_t level, uint8_t *data, uint8_t uid0, uint8_t u
 }
 
 st25r95_status_t st25r95_idle() {
-  tx_len = 0;
-  tx_buffer[tx_len++] = ST25_SEND;
-  tx_buffer[tx_len++] = ST25_IDLE;
-  tx_buffer[tx_len++] = 0x0E;
-  tx_buffer[tx_len++] = ST25_WU_SRC_TagDetection;
-  tx_buffer[tx_len++] = ST25_EC_TagDetection >> 8;
-  tx_buffer[tx_len++] = ST25_EC_TagDetection & 0xFF;
-  tx_buffer[tx_len++] = ST25_WU_CTRL_TagDetection >> 8;
-  tx_buffer[tx_len++] = ST25_WU_CTRL_TagDetection & 0xFF;
-  tx_buffer[tx_len++] = ST25_LEAVE_CTRL_TagDetection >> 8;
-  tx_buffer[tx_len++] = ST25_LEAVE_CTRL_TagDetection & 0xFF;
-  tx_buffer[tx_len++] = 0x20;
-  tx_buffer[tx_len++] = 0x60;
-  tx_buffer[tx_len++] = 0x60;
-  tx_buffer[tx_len++] = DACRef - 8;
-  tx_buffer[tx_len++] = DACRef + 8;
-  tx_buffer[tx_len++] = 0x3F;
-  tx_buffer[tx_len++] = 0x01;
+  tx_buffer[0] = ST25_SEND;
+  tx_buffer[1] = ST25_IDLE;
+  tx_buffer[2] = 0x0E;
+  tx_buffer[3] = ST25_WU_SRC_TagDetection;
+  tx_buffer[4] = ST25_EC_TagDetection >> 8;
+  tx_buffer[5] = ST25_EC_TagDetection & 0xFF;
+  tx_buffer[6] = ST25_WU_CTRL_TagDetection >> 8;
+  tx_buffer[7] = ST25_WU_CTRL_TagDetection & 0xFF;
+  tx_buffer[8] = ST25_LEAVE_CTRL_TagDetection >> 8;
+  tx_buffer[9] = ST25_LEAVE_CTRL_TagDetection & 0xFF;
+  tx_buffer[10] = 0x20;
+  tx_buffer[11] = 0x60;
+  tx_buffer[12] = 0x60;
+  tx_buffer[13] = DACRef - 8;
+  tx_buffer[14] = DACRef + 8;
+  tx_buffer[15] = 0x3F;
+  tx_buffer[16] = 0x01;
+  tx_len = 17;
 
   st25r95_nss(1);
   st25r95_spi_tx();
@@ -378,7 +376,7 @@ void st25r95_calibrate() {
   static uint8_t *res;
 
   for (uint8_t i = 0; i < 9; i++) {
-    tx_len = 0x11;
+    tx_len = 17;
 
     memcpy(tx_buffer, calibrate_data, sizeof(calibrate_data));
     st25r95_nss(1);
